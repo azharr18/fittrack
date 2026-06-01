0177ff16d3ac891e715a5a1d9bbcba6f575720c2
muhammad azhar ibrahim <147295782+azharr18@users.noreply.github.com>
2026-06-01 07:57:05 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ const User = db.define('users', {
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
-    age: { type: DataTypes.INTEGER },         // <-- TAMBAHKAN INI
-    height_cm: { type: DataTypes.FLOAT },     // <-- TAMBAHKAN INI
+    age: { type: DataTypes.INTEGER },         
+    height_cm: { type: DataTypes.FLOAT },    
     weight_kg: { type: DataTypes.FLOAT }
 }, {
     timestamps: false
