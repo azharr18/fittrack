d6658cf07ec330d53dc78d9a23bcbf730d47523c
muhammad azhar ibrahim <147295782+azharr18@users.noreply.github.com>
2026-06-01 07:50:42 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const db = new Sequelize('fitness_tracker', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
-    logging: false // Matikan log SQL di terminal agar bersih
+    logging: false 
 });
 
 module.exports = db;
